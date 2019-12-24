7f2aae35fd3ffa5872c66384b3e25c0ebcfca09f
ludike23 <ludike23@gmail.com>
2019-12-24 21:43:24 +0100

    resolved

--- routes/users.js
+++ routes/users.js
@@ -229,11 +229,11 @@ router.post("/uploadpics", multipartMiddleware, function (req, res) {
         cloudinary.uploader.upload(thumbFile,
             function (error, thumbnail) {
                 console.log(thumbnail);
-                let sql = "update users set profilePics ='" + thumbnail.secure_url + "', publicId= ? where id = " + authen.user.id;
+                let sql = "update users set profilePics ='" + thumbnail.secure_url + "', publicId='" + thumbnail.public_id+ "' where id = " + authen.user.id;
 
               //  let sql = `update users set profilePics = ${thumbnail.secure_url}, publicId = ${thumbnail.public_id} where id = ${authen.user.id}`;
                // let sql = "update users set profilePics ='" + thumbnail.secure_url + '", publicId="' + thumbnail.public_id + '" where id =' + authen.user.id;
-                connection.query(sql,thumbnail.public_id, function (err, results) {
+                connection.query(sql, function (err, results) {
                     if (err) {
                         res.status(503);
                         res.json(err);
